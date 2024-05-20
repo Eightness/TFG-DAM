f2c296a1dd5d4e6bdfc72357e54edc8544e77587
Albert <albertlb08@gmail.com>
2024-05-20 18:34:07 +0200

    first backend commit

---
+++ backend/poketeambuilder/src/main/java/albert/lozano/poketeambuilder/PoketeambuilderApplication.java
@@ -0,0 +1,13 @@
+package albert.lozano.poketeambuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoketeambuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoketeambuilderApplication.class, args);
+	}
+
+}
---
+++ backend/poketeambuilder/src/test/java/albert/lozano/poketeambuilder/PoketeambuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package albert.lozano.poketeambuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoketeambuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
